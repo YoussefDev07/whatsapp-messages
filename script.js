9bec4c2987fd23919ee819c4d2f880e827e2831e
Youssef Over <99287103+YoussefOver@users.noreply.github.com>
2023-03-14 08:32:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// media
+
+const message = "Send Whatsapp Messages To Any Number Without Add it To Contacts!\nارسل رسائل واتساب إلى أي رقم دون إضافته إلى جهات الاتصال!".replace(/\n/g, "%0D%0A").replace(/ /g, "%20");
+const url = "file:///C:/xampp/htdocs/www/Whatsapp%20Messages/index.html";
+
+$("#whatsapp").click(function(){
+  window.open("https://wa.me/?text=" + `${message}\n\n${url}`.replace(/\n/g, "%0D%0A"));
+});
+
+$("#facebook").click(function(){
+  window.open("https://www.facebook.com/sharer/sharer.php?u=" + url);
+});
+
+$("#massenger").click(function(){
+  window.open("fb-messenger://share/?link=" + url);
+});
+
+$("#twitter").click(function(){
+  window.open("https://twitter.com/intent/tweet?text=" + message + "%0D%0A%0D%0A" + "&url=" + url);
+});
+
+$("#link").click(function(){
+  navigator.clipboard.writeText(message.replace(/%0D%0A/g, "\n").replace(/%20/g, " ") + "\n\n" + url).then(() => {
+    $(".copied").css("visibility", "visible");
+
+    setTimeout(function(){
+      $(".copied").css("visibility", "hidden");
+    }, 3000);
+  });
+});
+
+// w
+
+$("#number").change(function(){
+  let number = document.getElementById("number");
+  if (number.value.length < 8) {
+    $("#chat").attr("disabled", "");
+  } else {
+    $("#chat").removeAttr("disabled");
+  }
+});
+
+$("#chat").click(function(){
+  let phone = document.getElementById("number").value.replace("+", "").replace("(", "").replace(")", "").replace(/-/g, "").replace(/ /g, "");
+  window.location.href = "https://api.whatsapp.com/send?phone=" + phone;
+});
